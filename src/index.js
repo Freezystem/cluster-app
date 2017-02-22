3f900c9c5648140fc34ac48a94aa5db417c0f30a
Freezystem <freezystem@gmail.com>
2017-02-22 17:34:44 +0100

    add automatic fork on unexpected worker exit

--- src/index.js
+++ src/index.js
@@ -2,24 +2,32 @@ import cluster  from 'cluster';
 import os       from 'os';
 import colors   from './helpers/colors';
 
-let requestCount    = 0;
-const cpus          = os.cpus();
-const { log, warn } = console;
+let requestCount  = 0;
+const cpus        = os.cpus();
+const { log }     = console;
 const { 
   FgCyan, 
   FgBlue, 
   FgYellow,
+  FgGreen,
   Reset 
-}                   = colors;
+}                 = colors;
 
 if ( cluster.isMaster ) {
-  log(`Master cluster ${FgBlue}${process.pid}${Reset} setting up ${FgYellow}${cpus.length}${Reset} ${cpus.length > 1 ? 'workers' : 'worker'}`);
+  log(`Master(${FgBlue}${process.pid}${Reset}) setting up ${FgYellow}${cpus.length}${Reset} ${cpus.length > 1 ? 'workers' : 'worker'}`);
 
   cpus.forEach(() => cluster.fork());
 
-  cluster.on('online', worker => log(`Worker ${FgCyan}${worker.process.pid}${Reset} is online`));
+  cluster.on('fork', worker => log(`Forking worker(${FgCyan}${worker.process.pid}${Reset}) ${FgYellow}#${worker.id}${Reset}`));
+  cluster.on('online', worker => log(`Worker(${FgCyan}${worker.process.pid}${Reset}) ${FgYellow}#${worker.id}${Reset} is online`));
 
-  cluster.on('exit', ( worker, code, signal ) => warn(`Worker ${worker.process.pid} died`, code, signal));
+  cluster.on('exit', ( worker, code, signal ) => {
+    log(`Worker(${FgCyan}${worker.process.pid}${Reset}) ${FgYellow}#${worker.id}${Reset} exited (${signal || code})`);
+
+    if ( !worker.exitedAfterDisconnect ) {
+      cluster.fork();
+    }
+  });
 
   cluster.on('message', ( worker, message ) => {
     if ( message.cmd === 'notifyRequest' ) requestCount++;
@@ -29,6 +37,8 @@ if ( cluster.isMaster ) {
   });
 }
 else {
-  process.on('message', message => log(`${FgCyan}from master to worker(${Reset}${process.pid}${FgCyan}): ${message.cmd} ${FgYellow}#${message.requestCount}`));
+  process.on('message', message => {
+    log(`${FgCyan}from master to worker(${Reset}${process.pid}${FgCyan}): ${message.cmd} ${FgGreen}#${message.requestCount}`);
+  });
   require('./server');
 }
